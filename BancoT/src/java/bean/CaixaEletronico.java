80477ea7ed44ba295a34fd116f76796af2aa0b85
leandroJobDev <lealjobs88@gmail.com>
2019-11-08 16:05:43 -0200

    Get and Set banco, transacao e caixa eletronico

---
+++ BancoT/src/java/bean/CaixaEletronico.java
@@ -0,0 +1,26 @@
+
+package bean;
+
+public class CaixaEletronico {
+    
+    private String agencia, gerente;
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getGerente() {
+        return gerente;
+    }
+
+    public void setGerente(String gerente) {
+        this.gerente = gerente;
+    }
+    
+    
+    
+}
--- BancoT/src/java/bean/Endereco.java
+++ BancoT/src/java/bean/Endereco.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package bean;
 
-/**
- *
- * @author treinamento
- */
 public class Endereco extends Cliente {
     
     private int numero;
