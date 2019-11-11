11dbf75dc4ab573c8f69de5d31f7a48df20b077c
leandroJobDev <lealjobs88@gmail.com>
2019-11-11 16:09:02 -0200

    generalization

--- BancoT/src/java/utils/ConnectionFactory.java
+++ BancoT/src/java/utils/ConnectionFactory.java
@@ -1,14 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package utils;
 
-/**
- *
- * @author user
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionFactory {
-    
+
+    private static final String USER = "root";
+    private static final String PASSWD = "password";
+    private static final String URL = "jdbc:mysql://localhost:3306/Autentica";
+
+    public Connection getConnection() throws SQLException, ClassNotFoundException {
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, PASSWD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
